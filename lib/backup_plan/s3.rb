3e41d8036d0440fb7e38e0749696ad4bc5af1546
Jeff Wigal <jeff@wigaldesign.com>
2011-09-13 22:51:49 -0400

    add s3 push

--- lib/backup_plan/s3.rb
+++ lib/backup_plan/s3.rb
@@ -2,20 +2,17 @@ module BackupPlan
   class S3
     def self.upload_files
       establish_connection
-      rio(Config.upload_base).each do |file|
-        AWS::S3::S3Object.store(file.filename, open(file.filepath), Config.s3_bucket )
+      ::AWS::S3::Bucket.create Config.s3_bucket
+      rio(Config.upload_base).files(/^[^.]/).each do |file|
+        puts "Moving #{file.filename} to S3"
+        file.rm if ::AWS::S3::S3Object.store(file.filename.to_s, open(file.path), Config.s3_bucket )
       end
     end
-    
-    def self.create_bucket
-      establish_connection
-      AWS::S3::Bucket.create Config.s3_bucket
-    end
-    
+        
     private
     
-    def establish_connection
-      AWS::S3::Base.establish_connection!(
+    def self.establish_connection
+      ::AWS::S3::Base.establish_connection!(
         :access_key_id     => Config.s3_credentials.first,
         :secret_access_key => Config.s3_credentials.last
       )
